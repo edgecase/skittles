7d0513870fbd59ec45c46f97758c328e054334e4
Matt Darby <matt.darby@edgecase.com>
2011-08-25 14:26:45 -0400

    Blerg

--- lib/skittles/connection.rb
+++ lib/skittles/connection.rb
@@ -6,11 +6,7 @@ module Skittles
     private
 
     def connection
-      options = {:site => endpoint}
-
-      # Pass in :ssl options, etc.
-      options.merge(Skittles::RequestOptions) if Skittles::RequestOptions
-
+      options                 = {:site => endpoint}
       client                  = OAuth2::Client.new(client_id, client_secret, options)
       oauth_token             = OAuth2::AccessToken.new(client, access_token)
       oauth_token.token_param = 'oauth_token'
